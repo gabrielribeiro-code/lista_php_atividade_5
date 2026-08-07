168ca5d90ec058419e9f45428a7b20f734b47133
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-08-07 08:22:17 -0300

    feat: Criação de 3 funções inicias: -Contar letras maisculas, letras minusculas e contar números.

--- ex_1.php
+++ ex_1.php
@@ -1,15 +1,51 @@
 <?php
 
-function analisarSenha() {
+function contarLetrasMaiusculas($senha) {
+    preg_match_all('/[A-Z]/', $senha, $matches);
+    $quantidade = count($matches[0]);
+    return $quantidade;
 
+}
 
+function contarLetrasMinusculas($senha) {
+    preg_match_all('/[a-z]/', $senha, $matches);
+    $quantidade = count($matches[0]);
+    return $quantidade;
 
+}
 
+function contarNumeros($senha){
+    preg_match_all('/[0-9]/', $senha, $matches);
+    $quantidade = count($matches[0]);
+    return $quantidade;
 
 }
 
 
 
+function analisarSenha() {
+
+$senha = "gabe#2942AAa";
+
+echo "A senha é: " . $senha;
+
+$resultado = contarLetrasMaiusculas($senha);
+
+echo "<br><br> A senha possui " . $resultado . " letra(s) maiscula(s)";
+
+$resultado = contarLetrasMinusculas($senha);
+
+echo "<br><br> A senha possui " . $resultado . " letra(s) minuscula(s)";
+
+$resultado = contarNumeros($senha);
+
+echo "<br><br> A senha possui " . $resultado . " Numeros";
+
+
+}
+
+
+analisarSenha();
 
 
 

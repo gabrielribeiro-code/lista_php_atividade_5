78bdfbf519a010166a8de4f98a5b79ad949e2a99
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-08-08 15:29:46 -0300

    feat: primeira função da ativiade 3

--- ex_03.php
+++ ex_03.php
@@ -1,11 +1,59 @@
 <?php
 
+function contarConsultas($consultas){
+    $totalConsultas = count($consultas);
+    return $totalConsultas;
+}
+
+
+
+
+function especialidades(){
+
+}
+
+function pesqPaciente(){
+
+}
+function orgHorarios(){
+
+
+}
+
+
+
 
 
 
 function organizarAgenda(){
 
+$consultas = [
+    "paciente 1" => [
+        "nome" => "Bruno",
+        "especialidade" => "Odontologia",
+        "horario" => "15:00",
+        "data" => "02/10/2025"
+    ],
 
+     "paciente 2" => [
+        "nome" => "Davi",
+        "especialidade" => "Psicologa",
+        "horario" => "14:00",
+        "data" => "06/08/2024"
+    ],
+
+     "paciente 3" => [
+        "nome" => "Andre",
+        "especialidade" => "Consulta geral",
+        "horario" => "10:00",
+        "data" => "02/02/2020"
+    ],
+
+];
+
+$resultado = contarConsultas($consultas);
+
+echo "O total de consultas é: " . $resultado;
 
 }
 
